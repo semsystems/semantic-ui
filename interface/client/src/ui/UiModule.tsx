3ed543792cb7e0907bbf4c5091d1fd0b185e3086
MikhailSadovsky <mishachess@mail.ru>
2021-05-10 16:28:47 +0300

    fix(interface): correction of button display

--- interface/client/src/ui/UiModule.tsx
+++ interface/client/src/ui/UiModule.tsx
@@ -2,25 +2,16 @@ import * as React from 'react';
 import * as store from '../store';
 import { connect } from 'react-redux';
 import { ScAddr } from '@ostis/sc-core';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 
-import {
-    Layout,
-    Select,
-    Button as Btn,
-    Menu,
-    Switch,
-    Radio,
-    Input
-} from 'antd';
+import { Layout, Select, Button as Btn, Menu, Switch, Radio, Input } from 'antd';
 import { GooglePlusOutlined, PrinterOutlined, ArrowsAltOutlined, LinkOutlined } from '@ant-design/icons';
-import {resolveSrv} from "dns";
-
 
 const { Option } = Select;
 const { Header, Footer, Sider, Content } = Layout;
 const { Item } = Menu;
-const { Group, Button} = Radio;
+const { Group, Button } = Radio;
 
 export interface KnowledgeBaseProps {
     services: store.serv.Services;
@@ -37,16 +28,16 @@ export const Library = {
     button: Btn,
     menu: Menu,
     item: Item,
-    block: "div",
+    block: 'div',
     switch: Switch,
-    p: "p",
+    p: 'p',
     google: GooglePlusOutlined,
     printerIcon: PrinterOutlined,
     arrowsIcon: ArrowsAltOutlined,
     linkIcon: LinkOutlined,
     group: Group,
     btn: Button,
-    input: Input
+    input: Input,
 };
 
 function mapStateToProps(state: store.Store): KnowledgeBaseProps {
@@ -66,7 +57,7 @@ interface TInterface {
 }
 
 function Interface({ model, lib }: TInterface): JSX.Element | null {
-    const iterateProps = (style: Object): any => {
+    const iterateProps = (style: unknown): any => {
         const semanticProps = {
             textColor: 'color',
             textSize: 'fontSize',
@@ -76,11 +67,10 @@ function Interface({ model, lib }: TInterface): JSX.Element | null {
             height: 'height',
         };
 
-        const renameKeys = (keysMap: object, obj: object) =>
+        const renameKeys = (keysMap: unknown, obj: unknown) =>
             Object.keys(obj).reduce(
                 (acc, key) => ({
                     ...acc,
-                    // @ts-ignore
                     ...{ [keysMap[key] || key]: obj[key] },
                 }),
                 {},
@@ -89,22 +79,21 @@ function Interface({ model, lib }: TInterface): JSX.Element | null {
         return renameKeys(semanticProps, style);
     };
 
-    // @ts-ignore
     const iterate = (model: UIComponent[]) => {
         return model.map((e) => {
             const { component, text, ...prop } = e;
             const CSS = iterateProps({ ...prop });
-            return React.createElement(lib[component], { style: CSS, ...prop }, text);
+            return React.createElement(lib[component], { style: CSS, key: nanoid(), ...prop }, text);
         });
     };
 
     const Tree = iterate(model);
-    return <>{Tree}</>;
+    return <div key={nanoid()}>{Tree}</div>;
 }
 
 export const UiModuleImpl: React.FC<KnowledgeBaseProps> = (props: KnowledgeBaseProps) => {
     const { services } = props;
-    let result;
+    const [jsonLinkContent, setJsonLinkContent] = useState('[]');
     const loadUiJson = async () => {
         const addrs: ScAddr[] = await services.sc.utils.CallUiJsonTranslationAgent();
         const actionNode: ScAddr = addrs[0];
@@ -112,17 +101,15 @@ export const UiModuleImpl: React.FC<KnowledgeBaseProps> = (props: KnowledgeBaseP
         const componentNode: ScAddr = await services.sc.utils.SearchComponentNode(actionNode);
 
         await services.sc.utils.waitFinishedArc(componentNode);
-        const jsonLinkContent = await services.sc.utils.getUiJsonLinkContent(componentNode);
-        console.log(jsonLinkContent);
-        return jsonLinkContent;
+        const json = await services.sc.utils.getUiJsonLinkContent(componentNode);
+        console.log(json);
+        return json;
     };
     useEffect(() => {
-        loadUiJson().then((jsonLinkContent) => {
-            console.log('Agent finished');
-            result = Interface({ model: [JSON.parse(jsonLinkContent)], lib: Library });
+        loadUiJson().then((json) => {
+            setJsonLinkContent('[' + json + ']');
         });
     });
-
-    return result;
+    return <div>{jsonLinkContent != '[]' && <Interface model={JSON.parse(jsonLinkContent)} lib={Library} />} </div>;
 };
 export const UiModule = connect(mapStateToProps)(UiModuleImpl);
